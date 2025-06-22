a2498901a243af5e77057eca3c6ec84cdbca70bf
Mariano138 <agueromariano138@gmail.com>
2025-06-21 22:14:08 -0300

    Colors added

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -11,11 +11,20 @@ export default function ColorPicker({ setColor }: { setColor: Dispatch<SetStateA
   return (
     <View>
       <Pressable
-        onPress={() => handleColorPick('red')}
-        style={{ backgroundColor: 'red', padding: 10 }}></Pressable>
+        onPress={() => handleColorPick('#e8d3ef')}
+        style={{ backgroundColor: '#e8d3ef', padding: 10 }}></Pressable>
       <Pressable
-        onPress={() => handleColorPick('blue')}
-        style={{ backgroundColor: 'blue', padding: 10 }}></Pressable>
+        onPress={() => handleColorPick('#f0bccc')}
+        style={{ backgroundColor: '#f0bccc', padding: 10 }}></Pressable>
+      <Pressable
+        onPress={() => handleColorPick('#ffeee2')}
+        style={{ backgroundColor: '#ffeee2', padding: 10 }}></Pressable>
+      <Pressable
+        onPress={() => handleColorPick('#f7ffe0')}
+        style={{ backgroundColor: '#f7ffe0', padding: 10 }}></Pressable>
+      <Pressable
+        onPress={() => handleColorPick('#cbeada')}
+        style={{ backgroundColor: '#cbeada', padding: 10 }}></Pressable>
     </View>
   );
 }
--- hooks/useGenerateColor.ts
+++ hooks/useGenerateColor.ts
@@ -1,6 +1,6 @@
 export default function useGenerateColor() {
   //Hook que elije un color random para la nota si el usuario no selecciona uno.
-  const pastelColors = ['red', 'blue'];
+  const pastelColors = ['#e8d3ef', '#f0bccc', '#ffeee2', '#f7ffe0', '#cbeada'];
   const randomColor = pastelColors[Math.floor(Math.random() * pastelColors.length)];
   return randomColor;
 }
